cefa761f2131aabae5b469facdcc26079782a00d
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-01-11 21:04:34 +0100

    🚀 v2.0.0-alpha01

--- mockup-processor/build.gradle.kts
+++ mockup-processor/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.github.miroslavhybler.mockup-processor"
-version = "1.2.4"
+version = "2.0.0-alpha01"
 
 kotlin {
     jvmToolchain(jdkVersion = 11)
@@ -82,7 +82,7 @@ afterEvaluate {
                 from(components.getByName("kotlin"))
                 groupId = "com.github.miroslavhybler"
                 artifactId = "mockup-processor"
-                version = "2.0.0-ALPHA01"
+                version = "2.0.0-alpha01"
                 pom {
                     description.set("Jitpack.io deploy")
                 }
